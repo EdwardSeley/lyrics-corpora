b79046854b3640779da23a17b5369dffd6d4b95d
Edward Seley <edwardseley@gmail.com>
2018-01-23 18:42:49 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+long_description = open('README.md').read()
+
+setup(
+    name='lyrics-corpora',
+    version='0.0.1',
+    description='Lyrics API',
+    long_description=long_description,
+    install_requires=['billboard', 'requests', 'bs4'],
+    url='https://github.com/edwardseley/lyrics-corpora',
+    author='Edward Seley',
+    author_email='edwardseley@gmail.com',
+    license='No license',
+    py_modules=['lyrics-corpora'],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 4 - Beta',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Natural Language :: English',
+    ],
+    keywords='lyrics LyricWikia music billboard songs scrape',
+    entry_points={
+        'console_scripts': [
+            'lyrics-corpora=lyrics-corpora:main',
+        ],
+    },
+)
